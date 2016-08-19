a80ae2fd53e2847fef5d8661d0a31703a8b506ae
Naoya Nakazawa <naoya.n@gmail.com>
2016-08-19 18:40:41 +0900

    Upgrade hub 2.2.5

--- files/brews/hub.rb
+++ files/brews/hub.rb
@@ -1,16 +1,16 @@
-require 'formula'
-
 class Hub < Formula
-  homepage "http://hub.github.com/"
-  url "https://github.com/github/hub/archive/v2.2.1.tar.gz"
-  sha1 "c64e473a36aabf71ecc39395b88c6993d4e12873"
+  desc "Add GitHub support to git on the command-line"
+  homepage "https://hub.github.com/"
+  url "https://github.com/github/hub/archive/v2.2.5.tar.gz"
+  sha256 "653cb1efed6d39348f630472b68f79b11704f52fdd7aebd82f7a6dbe18c32c59"
+
   head "https://github.com/github/hub.git"
 
   bottle do
-    cellar :any
-    sha256 "ce82b60ae28c9d788e816276b12086b91e68c1c15b90b638fa380326e3846b66" => :yosemite
-    sha256 "b995e7d96af5d5ac27236fd3317b24eccb89cbb50b3c02a77c49d023d8d27334" => :mavericks
-    sha256 "00bfe4481c997341e41a564fa5f32abc87ca1ce735c7afede826c6d491863e81" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "546ade95cc6351c4e70eeb2ef5ccbcae36d9bd751ca000f313070893a405a819" => :el_capitan
+    sha256 "642dfcd6c6cde344a2cf98f820e4d3bf9c95120b2a88508503c674bf8c23eaa7" => :yosemite
+    sha256 "576dd3fd69abf000eb1e3eefa64a98a25eed69bef14b372b2de554c9f4b08b3e" => :mavericks
   end
 
   option "without-completions", "Disable bash/zsh completions"
@@ -18,17 +18,21 @@ class Hub < Formula
   depends_on "go" => :build
 
   def install
-    system "script/build"
+    system "script/build", "-o", "hub"
     bin.install "hub"
     man1.install Dir["man/*"]
 
-    bash_completion.install "etc/hub.bash_completion.sh"
-    zsh_completion.install "etc/hub.zsh_completion" => "_hub"
+    if build.with? "completions"
+      bash_completion.install "etc/hub.bash_completion.sh"
+      zsh_completion.install "etc/hub.zsh_completion" => "_hub"
+    end
   end
 
   test do
-    HOMEBREW_REPOSITORY.cd do
-      assert_equal "bin/brew", shell_output("#{bin}/hub ls-files -- bin").strip
-    end
+    system "git", "init"
+    %w[haunted house].each { |f| touch testpath/f }
+    system "git", "add", "haunted", "house"
+    system "git", "commit", "-a", "-m", "Initial Commit"
+    assert_equal "haunted\nhouse", shell_output("#{bin}/hub ls-files").strip
   end
 end
